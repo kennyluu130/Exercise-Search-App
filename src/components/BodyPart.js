5a0263e10de9ab3659dd203e009f0e8657ea1229
kennyluuuuuuu <kennyluu130@gmail.com>
2024-10-30 22:31:58 -0700

    changed icons

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Stack, Typography } from "@mui/material";
-import Icon from "../assets/icons/gym.png";
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => (
   <Stack
@@ -34,7 +33,6 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
       window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
     }}
   >
-    <img src={Icon} alt="dumbbell" style={{ width: "40px", height: "40px" }} />
     <Typography
       fontSize="24px"
       fontWeight="bold"
